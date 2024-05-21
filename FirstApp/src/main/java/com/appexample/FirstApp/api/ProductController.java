107a20231c3b88ca7de0b0cbd5db28f4f50c163f
Adriana <adellin1996@gmail.com>
2024-05-21 19:38:04 +0300

    updated

--- FirstApp/src/main/java/com/appexample/FirstApp/api/ProductController.java
+++ FirstApp/src/main/java/com/appexample/FirstApp/api/ProductController.java
@@ -4,9 +4,10 @@ import com.appexample.FirstApp.api.DTO.ProductDtoAdd;
 import com.appexample.FirstApp.api.DTO.ProductDtoUpdate;
 import com.appexample.FirstApp.domain.product.Product;
 import com.appexample.FirstApp.domain.product.ProductRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.appexample.FirstApp.api.exception.BadRequestException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -38,10 +39,14 @@ public class ProductController {
     @PostMapping("/update/{id}")
     Product update(
             @PathVariable Integer id,
-            @RequestBody ProductDtoUpdate updateDto){
+            @RequestBody ProductDtoUpdate updateDto) throws BadRequestException {
         Product productUpdated = productRepository.findById(id).
-                orElseThrow(() -> new RuntimeException("Nu exista produsul cu id-ul"+id));
+                orElseThrow(() -> new BadRequestException("Nu exista produsul cu id-ul: " + id));
+        /** verificare calcule price/vat */
 
+        if (updateDto.getPriceWithoutVAT() + updateDto.getVAT() != updateDto.getPriceWithVat()) {
+            throw new BadRequestException("Pretul fara tva+tva nu corespunde nu pretul cu tva!");
+        }
 
         productUpdated.setName(updateDto.getName());
         productUpdated.setPriceWithoutVAT(updateDto.getPriceWithoutVAT());
@@ -51,11 +56,47 @@ public class ProductController {
         return productRepository.save(productUpdated);
     }
 
+    /**
+     * Discount utilizator
+     */
 
-    /** DELETE endpoint */
+    @PostMapping("/checkout/discount/{id}")
+    Product discount(@PathVariable Integer id,
+                     @RequestParam String discount) {
+        Product productDiscount = productRepository.findById(id)
+                .orElseThrow(() -> new BadRequestException("Nu exista produsul cu id-ul: " + id));
+
+        double procentDiscount = 0;
+        if (discount == null || !discount.equals("java")) {
+            procentDiscount = 1;
+        } else {
+            procentDiscount = (double) 50 / 100;
+        }
+        productDiscount.setPriceWithoutVAT((int) (productDiscount.getPriceWithoutVAT() * procentDiscount));
+        productDiscount.setPriceWithVat((int) (productDiscount.getPriceWithVat() * procentDiscount));
+        productDiscount.setVAT((int) (productDiscount.getVAT() * procentDiscount));
 
 
-    /** GET/READ endpoint */
+        return productDiscount;
+
+    }
+
+
+    /**
+     * DELETE endpoint
+     */
+    @DeleteMapping("/delete/{id}")
+    ResponseEntity<String> delete(@PathVariable Integer id) {
+        Product productToBeDeleted = productRepository.findById(id).
+                orElseThrow(() -> new BadRequestException("Nu exista produsul cu id-ul: " + id));
+        productRepository.delete(productToBeDeleted);
+        return ResponseEntity.ok("The product have been deleted.");
+    }
+
+
+    /**
+     * GET/READ endpoint
+     */
     @GetMapping("/products")
     public List<Product> getAllProducts() {
         return productRepository.findAll();
